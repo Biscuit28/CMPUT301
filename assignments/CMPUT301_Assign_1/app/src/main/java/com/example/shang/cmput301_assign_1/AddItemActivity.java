e445e57eb968121792123263c74ac39db1a7ab2f
Biscuit28 <shanguk@gmail.com>
2017-09-10 18:33:54 -0700

    added a default value ability when creating a new counter. Now uses Shared preferences to store and communicate data between activities

--- assignments/CMPUT301_Assign_1/app/src/main/java/com/example/shang/cmput301_assign_1/AddItemActivity.java
+++ assignments/CMPUT301_Assign_1/app/src/main/java/com/example/shang/cmput301_assign_1/AddItemActivity.java
@@ -1,11 +1,17 @@
 package com.example.shang.cmput301_assign_1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -15,15 +21,41 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
     }
 
+    public String getCurrentString() {
+        SharedPreferences sharedPref = getSharedPreferences("book_counts", Context.MODE_PRIVATE);
+        String countBook = sharedPref.getString("data", "NULL");
+        return countBook;
+    }
+
+    public void newCounter(String title, String timeStamp, String defaultVal) {
+        SharedPreferences sharedPref = getSharedPreferences("book_counts", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        String newData = String.format("%s-%s-%s-%s", title, timeStamp, defaultVal, defaultVal);
+        String currData = getCurrentString();
+        if (currData != "NULL"){
+            editor.putString("data", currData + " " + newData);
+        }
+        else {
+            editor.putString("data", currData);
+        }
+        editor.apply();
+    }
+
     public void submitEntry(View view) {
         Intent afterSubmit = new Intent(this, MainActivity.class);
         EditText newTitle = (EditText) findViewById(R.id.new_title);
-        //afterSubmit.putStringArrayListExtra("new_item", );
+        EditText defaultVal_field = (EditText) findViewById(R.id.default_value);
         String title = newTitle.getText().toString();
+        String defaultVal = defaultVal_field.getText().toString();
         if (title.isEmpty()) {
             Toast.makeText(this, "Title cannot be empty!", Toast.LENGTH_SHORT).show();
         }
+        else if (defaultVal.isEmpty()){
+            Toast.makeText(this, "Default value cannot be empty!", Toast.LENGTH_SHORT).show();
+        }
         else {
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd-HH:mm").format(Calendar.getInstance().getTime());
+            newCounter(title, timeStamp, defaultVal);
             Toast.makeText(this, "added to countbook!", Toast.LENGTH_SHORT).show();
             startActivity(afterSubmit);
         }
--- assignments/CMPUT301_Assign_1/app/src/main/java/com/example/shang/cmput301_assign_1/MainActivity.java
+++ assignments/CMPUT301_Assign_1/app/src/main/java/com/example/shang/cmput301_assign_1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.shang.cmput301_assign_1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -12,16 +15,20 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> itemList = new ArrayList<String>();
 
+    Intent received = getIntent();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPref = getSharedPreferences("book_counts", Context.MODE_PRIVATE);
+        String countBook = sharedPref.getString("data", "NULL");
+        Toast.makeText(this, countBook, Toast.LENGTH_LONG).show();
         ListView myListView = (ListView) findViewById(R.id.listView);
     }
 
     /*method for creating a new item*/
     public void newEntry(View view) {
-        System.out.println("whussup");
         Intent new_item_intent = new Intent(this, AddItemActivity.class);
         startActivity(new_item_intent);
     }
